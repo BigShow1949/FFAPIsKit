decfc14e1bc6e8ace81b4a86aba8b441764fd209
big show <bigshow1949@icloud.com>
2019-02-20 10:48:11 +0700

    0.1.0

---
+++ Example/Pods/Target Support Files/FFAPIsKit/FFAPIsKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FFAPIsKit : NSObject
+@end
+@implementation PodsDummy_FFAPIsKit
+@end
---
+++ Example/Pods/Target Support Files/FFAPIsKit/FFAPIsKit-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "APIRequest.h"
+#import "APIRequestProtocol.h"
+#import "APIResponseProtocol.h"
+#import "FFReformProtocol.h"
+#import "NetworkHelper.h"
+
+FOUNDATION_EXPORT double FFAPIsKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char FFAPIsKitVersionString[];
+
